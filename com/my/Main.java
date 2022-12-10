8eefe4e88462775dfcfc238c419d6eec371b930c
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-12-10 18:02:57 +0300

    add homework

---
+++ com/my/Main.java
@@ -0,0 +1,20 @@
+/* Задание
+Дописать телефонный справочник:
+Создать телефонный справочник с возможностью импорта и
+экспорта данных в нескольких форматах.
+под форматами понимаем структуру файлов, например:
+- в файле на одной строке хранится одна часть записи, пустая
+строка - разделитель
+ */
+
+package com.my;
+
+import controller.Controller;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException {
+        Controller controller = new Controller();
+        controller.run();
+    }
+}
